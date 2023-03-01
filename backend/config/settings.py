118f4a3a5bd6ba88cb4411f3faec8256a50ac501
Not-user-1984 <deman.tmb.68@gmail.com>
2023-03-01 14:01:08 +0300

    deploy v0

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -98,14 +98,11 @@ else:
         }
     }
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 
 AUTH_PASSWORD_VALIDATORS = [
